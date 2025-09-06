4e82dcc513fdc8ecc7190ae9805804fd9faf98bc
Justin <spanosjustin96@gmail.com>
2025-09-06 13:52:59 -0400

    Scaffolding for the MAtrix/Inventory/Page views

---
+++ src/app/inventory/tree/page.tsx
@@ -0,0 +1,7 @@
+export default function MatrixPage() {
+    return (
+        <div className="p-6 text-xl font-bold">
+            Hello World, from the Inventory Tree Page!
+        </div>
+    );
+}
--- src/app/repair/page.tsx
+++ src/app/repair/page.tsx
@@ -1,7 +1,7 @@
 export default function MatrixPage() {
     return (
         <div className="p-6 text-xl font-bold">
-            Hello World, from the Inventory List Page!
+            Hello World, from the Repair Page!
         </div>
     );
 }
